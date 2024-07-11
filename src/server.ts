5ec193ccb419c9a0d0f43201a66f33202aa6250a
Pedro Sartor <pedro.sartor@easylive.com.br>
2024-07-11 14:59:49 -0300

    initial commit

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,20 @@
 import fastify from 'fastify'
+import { knexProvider } from './database'
+import crypto from 'node:crypto'
 
 const app = fastify()
-app.get('/hello', () => {
-  return 'Hello!'
+app.get('/hello', async () => {
+  const transaction = await knexProvider('transactions')
+    .insert({
+      id: crypto.randomUUID(),
+      title: 'Transação de teste',
+      amount: 1000,
+    })
+    .returning('*')
+
+  return transaction
 })
 
-app.listen({ port: 3333 }, () => {
+app.listen({ port: 3333 }).then(() => {
   console.log('Server is running on http://localhost:3333')
 })
